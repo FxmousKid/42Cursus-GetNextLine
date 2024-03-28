359a44c0b8f2537947827a2a04c91913af704c44
FxmousKid <iyan005@outlook.com>
2024-03-28 14:07:52 +0100

    Test for getnextline file

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,35 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:34:46 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/27 11:34:47 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/28 14:07:29 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static int	line_num;
+
+	line_num = 0;
+	line = malloc(sizeof(char) * get_line_size(fd, line_num));
+	if (line == NULL)
+		return (NULL);
+	
+
+	line_num++;
+	return (line);
+}
+
+
+int main(void)
+{
+	int fd = open("./testfile.txt", O_RDONLY);
+	printf("line 1 size is %d", get_line_size(fd, 1));
+	
+
+	close(fd);
+	return (0);
+}
