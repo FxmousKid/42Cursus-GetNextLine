6cedc4ec900dfd512de1ca97a23f13ff1a74d999
FxmousKid <iyan005@outlook.com>
2024-03-30 15:03:33 +0100

    Commiting working version of GNL (not normed and valgrinded)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:34:46 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/29 00:29:24 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/30 15:02:32 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,17 @@
 
 char	*get_next_line(int fd)
 {
-	char			*tmp;
 	static char		*current_line;
+	char			**eol_split;
+	char			*tmp;
 	char			*buffer_line;
 	int				nl_pos;
-	char			**eol_split;
-
-	if(nl_detection(current_line) != 1)
+	
+	if (nl_detection(current_line) != -1) {
 		buffer_line = current_line;
-	else
+		current_line = NULL;
+	}
+	else 
 		buffer_line = malloc_line(fd);
 	while (buffer_line != NULL)
 	{
@@ -36,21 +38,32 @@ char	*get_next_line(int fd)
 			free(tmp);
 			if (current_line == NULL)
 				return (free(buffer_line), NULL);
+			free(buffer_line);
 			buffer_line = malloc_line(fd);
 			continue;
 		}
 		eol_split = split(buffer_line, nl_pos);
 		tmp = ft_strjoin(current_line, eol_split[0]);
 		current_line = eol_split[1];
-		return (tmp);
+		free(eol_split);
+		return (free(buffer_line), tmp);
 	}
 	return (NULL);
 }
 
+
+
 int main(void)
 {
 	int fd = open("./testfile.txt", O_RDONLY);
-
+	
+	char *tab = get_next_line(fd);
+	while (tab != NULL)
+	{
+		printf("%s", tab);
+		free(tab);
+		tab = get_next_line(fd);
+	}
 
 	close(fd);
 	return (0);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:35:04 by inazaria          #+#    #+#             */
-/*   Updated: 2024/03/29 00:05:30 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/03/30 14:58:37 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	nl_detection(char *tab)
 	int	counter;
 	
 	counter = 0;
-	while (tab[counter])
+	while (tab && tab[counter])
 	{
 		if (tab[counter] == '\n')
 			return (counter);
@@ -45,7 +45,7 @@ size_t ft_strlen(char *str) {
 	int index;
 
 	index = 0;
-	while(str[index])
+	while(str && str[index])
 		index ++;
 	return (index);
 }
@@ -57,7 +57,7 @@ static char	*ft_memcpy(char *dest, char *s1)
 
 	tmp = dest;
 	counter = 0;
-	while(s1[counter])
+	while(s1 && s1[counter])
 		*tmp++ = s1[counter++];
 	*tmp = 0;
 	return (tmp);
@@ -67,14 +67,15 @@ char	*ft_strjoin(char *s1, char *s2)
 {
 	char	*tab;
 	int		size_malloc;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	char	*tmp;
+	//if (s1 == NULL || s2 == NULL)
+	//	return (NULL);
 	size_malloc = ft_strlen(s1) + ft_strlen(s2);
 	tab = (char *) malloc(sizeof(char) * (size_malloc + 1));
 	if (tab == NULL)
 		return (NULL);
-	ft_memcpy(ft_memcpy(tab, s1), s2);
+	tmp = ft_memcpy(ft_memcpy(tab, s1), s2);
+	*tmp = 0;
 	return (tab);
 }
 
@@ -86,7 +87,7 @@ char **split(char *str, int pos) {
 		return (NULL);
 	str[pos] = '\0';
 	output[0] = ft_strjoin(str, "\n");
-	output[1] = ft_strjoin(str + pos, "");
+	output[1] = ft_strjoin(str + pos + 1, "");
 	return (output);
 }
 
