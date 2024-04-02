95f68361b1aefb058e1c83164432c5b6f44cef5f
FxmousKid <iyan005@outlook.com>
2024-04-02 19:03:52 +0200

    get_next_line_bonus working but doesnt pass all the tests, to look into after printf

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: inazaria <inazaria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 11:34:46 by inazaria          #+#    #+#             */
-/*   Updated: 2024/04/01 17:44:32 by inazaria         ###   ########.fr       */
+/*   Updated: 2024/04/02 18:52:05 by inazaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 char	*get_next_line_aux(int fd)
 {
-	static char		*current_line[1024];
+	static char		*current_line[2048];
 	char			**eol_split;
 	char			*tmp;
 	char			*buffer_line;
@@ -25,7 +25,7 @@ char	*get_next_line_aux(int fd)
 	else
 		buffer_line = malloc_line(fd);
 	current_line[fd] = calc_current_line(current_line[fd]);
-	while (buffer_line != NULL && buffer_line[0] != '\0')
+	while (buffer_line != NULL)
 	{
 		if (nl_detection(buffer_line) == -1)
 		{
@@ -35,23 +35,23 @@ char	*get_next_line_aux(int fd)
 		}
 		eol_split = split(buffer_line, nl_detection(buffer_line));
 		tmp = conc_cur_line(current_line[fd], eol_split[0]);
-		current_line[fd] = eol_split[1];
-		return (free(eol_split), tmp);
+		return (current_line[fd] = eol_split[1], free(eol_split), tmp);
 	}
-	return (free(buffer_line), NULL);
+	tmp = current_line[fd];
+	return (current_line[fd] = NULL, free(buffer_line), tmp);
 }
 
 char	*get_next_line(int fd)
 {
 	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
-		return (NULL);
+		return (0);
 	return (get_next_line_aux(fd));
 }
 
 char	*calc_current_line(char	*current_line)
 {
 	if (nl_detection(current_line) != -1)
-		return (0);
+		return (NULL);
 	return (current_line);
 }
 
@@ -60,13 +60,14 @@ char	*conc_cur_line(char *current_line, char *buffer_line)
 	char	*tmp;
 
 	tmp = current_line;
-	current_line = ft_strjoin(tmp, buffer_line);
 	if (current_line == NULL)
 	{
+		current_line = ft_strjoin("", buffer_line);
 		free(tmp);
 		free(buffer_line);
-		return (NULL);
+		return (current_line);
 	}
+	current_line = ft_strjoin(tmp, buffer_line);
 	free(tmp);
 	free(buffer_line);
 	return (current_line);
@@ -81,29 +82,8 @@ char	*malloc_line(int fd)
 	if (line == NULL)
 		return (NULL);
 	read_status = read(fd, line, BUFFER_SIZE);
-	if (read_status < 0)
+	if (read_status <= 0)
 		return (free(line), NULL);
 	line[read_status] = '\0';
 	return (line);
 }
-/*
-int main(void)
-{
-	int fd = open("./testfile.txt", O_RDONLY);
-	int fd1 = open("./testfile2.txt", O_RDONLY);
-
-	char *tab = get_next_line(fd);
-	char *tab1 = get_next_line(fd1);
-	while (tab != NULL)
-	{
-		printf("%s", tab);
-		printf("%s", tab1);
-		free(tab);
-		free(tab1);
-		tab = get_next_line(fd);
-		tab1 = get_next_line(fd1);
-	}
-	free(tab);
-	close(fd);
-	return (0);
-}*/
